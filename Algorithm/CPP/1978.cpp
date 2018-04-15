44e91d0a47040c2d3474fecb020d39b68a22b075
JungHa-Cho <cjh8487@naver.com>
2018-04-15 21:52:55 +0900

    20180415-isPrime
    20180415-isPrime

--- Algorithm/CPP/1978.cpp
+++ Algorithm/CPP/1978.cpp
@@ -2,33 +2,41 @@
 
 using namespace std;
 
+bool isPrime(int n) 
+{
+    if (n <= 1)
+        return false;
+    
+    if ( (n & 1) == 0 )
+        return (n == 2);
+    
+    for (int i = 3; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;    
+}
+
 int main(void)
 {
     int t;
-    int n[101];
+    int n[101] = { 0, };
     int primeCount = 0;
     int divideCount = 0;
+
     scanf("%d", &t);
-
-    for (int i = 0; i < t; i++) {
-        scanf("%d", &n);
+    for (int i = 0; i < t; i++) 
+    {
+        scanf("%d", &n[i]);
     }
-
 
     for (int k = 0; k < t; k++) 
     {
-        for (int j = 1; j < n[k]; j++)
-        {
-            if (j % n[k] == 0)
-            {
-                divideCount++;
-                if (divideCount>1)
-                    break;
-            }
-            if (j == n[k] - 1 && divideCount < 2) {
-                primeCount++;
-            }
-        }
+        if (isPrime(n[k]))
+            primeCount++;
     }
     printf("%d\n", primeCount);
 }
